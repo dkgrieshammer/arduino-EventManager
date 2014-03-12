c51bb47b039fdef8b0fb620150f6411598fef24b
David <info@dkgrieshammer.de>
2014-03-12 19:19:09 +0100

    Update EventManager.cpp
    changed line 484 __iRestore(sregSave) to __iRestore(&sregSave) to reference to prevent pointer error

--- EventManager/EventManager.cpp
+++ EventManager/EventManager.cpp
@@ -481,7 +481,7 @@ boolean EventManager::EventQueue::queueEvent( int eventCode, int eventParam )
     if ( mInterruptSafeMode )
     {   
         // Restore previous state of interrupts 
-        __iRestore( sregSave );
+        __iRestore( &sregSave ); //changed to reference to prevent pointer error
     }
 
 #if EVENTMANAGER_DEBUG
